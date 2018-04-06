dc8263fc199f5e7af75a69a2a7f095e26c8efb44
WildC <s180026@student.dtu.dk>
2018-04-06 14:37:57 +0200

    Initial commit

--- myFlaskWebProject/views.py
+++ myFlaskWebProject/views.py
@@ -2,6 +2,9 @@
 Routes and views for the flask application.
 """
 
+from forms import VoteForm
+import config_cosmos
+import pydocumentdb.document_client as document_client
 from datetime import datetime
 from flask import render_template
 from myFlaskWebProject import app
@@ -9,11 +12,37 @@ from myFlaskWebProject import app
 @app.route('/')
 @app.route('/home')
 def home():
-    """Renders the home page."""
+    client = document_client.DocumentClient(config_cosmos.COSMOSDB_HOST, {'masterKey': config_cosmos.COSMOSDB_KEY})
+    # Read databases and take first since id should not be duplicated.
+    db = next((data for data in client.ReadDatabases() if data['id'] == config_cosmos.COSMOSDB_DATABASE))
+
+    # Read collections and take first since id should not be duplicated.
+    coll = next((coll for coll in client.ReadCollections(db['_self']) if coll['id'] == config_cosmos.COSMOSDB_COLLECTION))
+
+    # Read documents and take first since id should not be duplicated.
+    doc = next((doc for doc in client.ReadDocuments(coll['_self']) if doc['id'] == config_cosmos.COSMOSDB_DOCUMENT))
+
+    # Create a model to pass to results.html
+    class VoteObject:
+        choices = dict()
+        total_votes = 0
+
+    vote_object = VoteObject()
+    vote_object.choices = {
+        "Web Site" : doc['Web Site'],
+        "Cloud Service" : doc['Cloud Service'],
+        "Virtual Machine" : doc['Virtual Machine']
+    }
+    vote_object.total_votes = sum(vote_object.choices.values())
+    # Hack to avoid zero detection in empty database
+    if vote_object.total_votes == 0:
+        vote_object.total_votes = 1
+
     return render_template(
-        'index.html',
-        title='Home Page',
+        'results.html',
+        title='Home page',
         year=datetime.now().year,
+        vote_object = vote_object
     )
 
 @app.route('/contact')
@@ -26,12 +55,102 @@ def contact():
         message='Your contact page.'
     )
 
-@app.route('/about')
-def about():
+@app.route('/help')
+def help():
     """Renders the about page."""
     return render_template(
-        'about.html',
-        title='About',
+        'help.html',
+        title='Help',
         year=datetime.now().year,
         message='Your application description page.'
     )
+
+@app.route('/profile')
+def profile():
+    """Renders the about page."""
+    return render_template(
+        'profile.html',
+        title='Profile',
+        year=datetime.now().year,
+        message='Your profile.'
+    )
+
+@app.route('/clear')
+def clear():
+    """Renders the contact page."""
+    client = document_client.DocumentClient(config_cosmos.COSMOSDB_HOST, {'masterKey': config_cosmos.COSMOSDB_KEY})
+
+    # Attempt to delete the database.  This allows this to be used to recreate as well as create
+    try:
+        db = next((data for data in client.ReadDatabases() if data['id'] == config_cosmos.COSMOSDB_DATABASE))
+        client.DeleteDatabase(db['_self'])
+    except:
+        pass
+
+    # Create database
+    db = client.CreateDatabase({ 'id': config_cosmos.COSMOSDB_DATABASE })
+
+    # Create collection
+    collection = client.CreateCollection(db['_self'],{ 'id': config_cosmos.COSMOSDB_COLLECTION })
+
+    # Create document
+    document = client.CreateDocument(collection['_self'],
+        { 'id': config_cosmos.COSMOSDB_DOCUMENT,
+          'Web Site': 0,
+          'Cloud Service': 0,
+          'Virtual Machine': 0,
+          'name': config_cosmos.COSMOSDB_DOCUMENT 
+        })
+
+    return render_template(
+       'clear.html',
+        title='Clear',
+        year=datetime.now().year
+    )
+
+@app.route('/create', methods=['GET', 'POST'])
+def create(): 
+    form = VoteForm()
+    replaced_document ={}
+    if form.validate_on_submit(): # is user submitted vote  
+        client = document_client.DocumentClient(config_cosmos.COSMOSDB_HOST, {'masterKey': config_cosmos.COSMOSDB_KEY})
+
+        # Read databases and take first since id should not be duplicated.
+        db = next((data for data in client.ReadDatabases() if data['id'] == config_cosmos.COSMOSDB_DATABASE))
+
+        # Read collections and take first since id should not be duplicated.
+        coll = next((coll for coll in client.ReadCollections(db['_self']) if coll['id'] == config_cosmos.COSMOSDB_COLLECTION))
+
+        # Read documents and take first since id should not be duplicated.
+        doc = next((doc for doc in client.ReadDocuments(coll['_self']) if doc['id'] == config_cosmos.COSMOSDB_DOCUMENT))
+
+        # Take the data from the deploy_preference and increment our database
+        doc[form.deploy_preference.data] = doc[form.deploy_preference.data] + 1
+        replaced_document = client.ReplaceDocument(doc['_self'], doc)
+
+        # Create a model to pass to results.html
+        class VoteObject:
+            choices = dict()
+            total_votes = 0
+
+        vote_object = VoteObject()
+        vote_object.choices = {
+            "Web Site" : doc['Web Site'],
+            "Cloud Service" : doc['Cloud Service'],
+            "Virtual Machine" : doc['Virtual Machine']
+        }
+        vote_object.total_votes = sum(vote_object.choices.values())
+
+        return render_template(
+            'results.html', 
+            year=datetime.now().year, 
+            vote_object = vote_object
+        )
+
+    else :
+        return render_template(
+            'create.html', 
+            title = 'Create',
+            year=datetime.now().year,
+            form = form
+        )
