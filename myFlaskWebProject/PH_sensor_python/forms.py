fc9d0fe19195b4339a05a8d8c93310ada8e0e531
Montoya <manuwhs@bitbucket.org>
2018-04-20 17:15:51 +0200

    Code for reading PH

--- myFlaskWebProject/PH_sensor_python/forms.py
+++
@@ -1,8 +0,0 @@
-from flask.ext.wtf import Form
-from wtforms import RadioField
-
-class VoteForm(Form):
-    deploy_preference  = RadioField('Deployment Preference', choices=[
-        ('Web Site', 'Web Site'),
-        ('Cloud Service', 'Cloud Service'),
-        ('Virtual Machine', 'Virtual Machine')], default='Web Site')
